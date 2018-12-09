d356df16ab16ad2aa34f5cb8273ca1bd877df623
skachkovm <skachkov.m@roistat.com>
2018-12-09 14:37:03 +0300

    Uploading the laravel carcase

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('title')
+    Home - Shadowlands
+    @endsection
+
+@section('content')
+    <h1>This is the home page!</h1>
+    @endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+@include('inc.navbar')
+@yield('content')
+</body>
+</html>
